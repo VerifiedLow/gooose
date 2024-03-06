b87f5eff48520c47983fd0df4f375f6d1634fdfe
VerifiedLow <162498553+VerifiedLow@users.noreply.github.com>
2024-03-06 12:26:19 -0700

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,85 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level0":
+            case "level2":return tiles.createTilemap(hex`1000100009090909090c0c0c0c0c0c0c0c0c0c090c02010101010101010101010101050b0c0306060606060606060606060604090c03060606060606060606060606040c0b03060606060606060606060606040c0c03060606060606060606060606040c0c03060606060606060606060606040c0903060606060606060606060606040c0903060606060606060606060606040c0903060606060606060606060606040c0903060606060606060606060606040c0f0e0e0e0e0e0e0e0e0e0e0e0e0e0e0d0a0a0a0a070708070a0a0a080a07070a0a081007070a0a0a0a0a070a0a1007080a0a0a0a0a070a0708100a0a0a0a0a0a0a0a0a0a0a070a0a0a0a0a0a0a080a0a`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath2,sprites.castle.tilePath1,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath3,sprites.castle.tilePath5,sprites.castle.tileDarkGrass1,sprites.castle.tileDarkGrass2,sprites.castle.tileGrass3,sprites.castle.tileDarkGrass3,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.skillmap.islandTile8,sprites.skillmap.islandTile7,sprites.skillmap.islandTile6,sprites.swamp.swampTile2,myTiles.tile3], TileScale.Sixteen);
+            case "level":
+            case "level1":return tiles.createTilemap(hex`1000100009090909090c0c0c0c0c0c0c0c0c0c090c02010101010101010101010101050b0c0306060606060606060606060604090c03060606060606060606060606040c0b03060606060606060606060606040c0c03060606060606060606060606040c0c03060606060606060606060606040c0903060606060606060606060606040c0903060606060606060606060606040c0903060606060606060606060606040c0903060606060606060606060606040c0f0e0e0e0e0e0e0e0e0e0e0e0e0e0e0d0a0a0a0a070708070a0a0a080a07070a0a081007070a0a0a0a0a070a0a1007080a0a0a0a0a070a0708100a0a0a0a0a0a0a0a0a0a0a070a0a0a0a0a0a0a080a0a`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath2,sprites.castle.tilePath1,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath3,sprites.castle.tilePath5,sprites.castle.tileDarkGrass1,sprites.castle.tileDarkGrass2,sprites.castle.tileGrass3,sprites.castle.tileDarkGrass3,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.skillmap.islandTile8,sprites.skillmap.islandTile7,sprites.skillmap.islandTile6,sprites.swamp.swampTile2], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "chest":
+            case "tile1":return tile1;
+            case "myTile":
+            case "tile3":return tile3;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
